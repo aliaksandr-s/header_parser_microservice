27531f8856cc16148a2ba591cb3ca4aaa98bc139
aliaksandr-s <s-a-n-9@mail.ru>
2016-11-08 00:06:47 +0300

    commit

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', function (req, res) {
-    res.send('hello')
+app.get('*', function (req, res) {
+    res.json({
+        ip: req.ip,
+        language: req.headers['accept-language'].split(',')[0],
+        os: req.headers['user-agent'].match(/\(([^\)]+)\)/)[1]
+    })
 })
 
 app.listen(port, function () {

a2c9c67679b62a6b979b49e0f886b039cc5e3b41
aliaksandr-s <s-a-n-9@mail.ru>
2016-11-08 00:18:06 +0300

    ip regexp

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('*', function (req, res) {
+    console.log(req.ip)
     res.json({
-        ip: req.ip,
+        ip: req.ip.match(/\d.+/)[0],
         language: req.headers['accept-language'].split(',')[0],
         os: req.headers['user-agent'].match(/\(([^\)]+)\)/)[1]
     })
